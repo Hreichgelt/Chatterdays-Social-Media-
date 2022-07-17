83de0b4e80961530b4af84cdcd079ebaa0176ff0
Hans Reichgelt <hreichgelt06@gmail.com>
2022-07-17 19:30:04 -0400

    updates

--- controllers/thoughtControll.js
+++ controllers/thoughtControll.js
@@ -1,2 +1,29 @@
 const { Thought, User } = require('../models');
 
+const thoughtControll = {
+    getThoughts(req, res) {
+        Thought.find()
+        .sort({ createdAt: -1 })
+        .then((thoughtData) => {
+            res.json(thoughtData);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        })
+    },
+
+    getOneThought(req, res) {
+        Thought.findOne({ _id: req.params.thoughtId })
+        .then((thoughtData) => {
+            if (!thoughtData) {
+                return res.status(404).json({ message: 'No Thoughts' });
+            }
+            res.json(thoughtData);
+        })
+        .catch((er) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+    }
+}
--- controllers/userControll.js
+++ controllers/userControll.js
@@ -1,1 +1,32 @@
 const { User, Thought } = require('../models');
+
+const userControll = {
+    getUsers(req, res) {
+        User.find()
+        .select('-__v')
+        .then((UserData) => {
+            res.json(UserData);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        })
+    },
+
+    getOneUser(req, res) {
+        User.findOne({ _id: req.params.userId })
+        .select('-__v')
+        .populate('friends')
+        .populate('thoughts')
+        .then((UserData) => {
+            if (!UserData) {
+                return res.status(404).json({ message: 'No User' });
+            }
+            res.json(UserData);
+        })
+        .catch((er) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+    }
+}
